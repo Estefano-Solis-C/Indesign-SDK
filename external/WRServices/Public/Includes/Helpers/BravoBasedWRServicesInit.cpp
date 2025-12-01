fa27c6c3c5e97bd1e032e53fe3fb833b5a50d99d
Gaftherman <fano.solis.campos@gmail.com>
2025-12-01 00:58:30 -0500

    Dejar de rastrear carpetas locales

--- external/WRServices/Public/Includes/Helpers/BravoBasedWRServicesInit.cpp
+++
@@ -1,5 +0,0 @@
-
-WRUInt32 BravoBasedWRServicesInit::fPreInitCount = 0;
-WRMemObj *BravoBasedWRServicesInit::fMemObj = NULL;
-BravoBasedMultiProcessing *BravoBasedWRServicesInit::fMultiProcessing = NULL;
-IWRFontAccess *BravoBasedWRServicesInit::fFontAccess = NULL;
